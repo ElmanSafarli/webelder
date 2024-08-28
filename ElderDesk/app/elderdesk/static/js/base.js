52e653824d96900d05ec1a9d9c1618845cfe851d
Elman Seferli <itiselman@gmail.com>
2024-08-29 03:05:52 +0400

    update

--- ElderDesk/app/elderdesk/static/js/base.js
+++ ElderDesk/app/elderdesk/static/js/base.js
@@ -5,13 +5,60 @@ $(document).ready(function(){
     });
 
     var $nav = $('.bottom-navigation');
-    var offsetTop = $nav.offset().top;
+    
+    if ($nav.length > 0) {
+        var offsetTop = $nav.offset().top;
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > offsetTop) {
-            $nav.addClass('fixed');
-        } else {
-            $nav.removeClass('fixed');
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > offsetTop) {
+                $nav.addClass('fixed');
+            } else {
+                $nav.removeClass('fixed');
+            }
+        });
+    }
+     
+    $(".btn_dashboard").click(function() {
+        var target = $(this).attr('id');
+        
+        $(".main-content-section").hide();
+        $("#" + target + "-section").show();
+        
+        $('.left-block nav>div button').removeClass('active');
+        $(this).addClass('active');
+        
+        localStorage.setItem('activeSection', target);
+    });
+
+    var savedSection = localStorage.getItem('activeSection');
+    
+    if (savedSection) {
+        $(".main-content-section").hide();
+        $("#" + savedSection + "-section").show();
+        
+        $('.left-block nav>div button').removeClass('active');
+        $("#" + savedSection).addClass('active');
+    } else {
+        $("#views-section").show();
+    }
+
+    $('#profile-button').click(function() {
+        $('#profile-menu').slideToggle(300); 
+    });
+
+    $('#add-organization').click(function() {
+        $('.shadow-background').fadeIn();
+        $('.add-organization-form').fadeIn();
+    });
+
+    $('.organization-form-close-btn, .shadow-background').click(function() {
+        $('.shadow-background').fadeOut();
+        $('.add-organization-form').fadeOut();
+    });
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest('#profile-button').length && !$(event.target).closest('#profile-menu').length) {
+            $('#profile-menu').slideUp(300);
         }
     });
 });
--- ElderDesk/app/elderdesk/tickets/views.py
+++ ElderDesk/app/elderdesk/tickets/views.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from organizations.models import Organization
+from organizations.forms import OrganizationForm
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'ticket/dashboard.html'
     login_url = '/accounts/login/'  
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['organizations'] = Organization.objects.all()
+        context['organization_form'] = OrganizationForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = OrganizationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')  # Assuming 'dashboard' is the name of your dashboard URL pattern
+        return self.get(request, *args, **kwargs)
